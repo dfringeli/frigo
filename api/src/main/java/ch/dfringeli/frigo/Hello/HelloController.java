3c0acaab732dab10dafe135719a27a45d512cc7a
Dominik Fringeli <dominik.fringeli.ext@hubacontrol.com>
2025-05-23 13:26:57 +0000

    added cors updated spring boot version

--- api/src/main/java/ch/dfringeli/frigo/Hello/HelloController.java
+++ api/src/main/java/ch/dfringeli/frigo/Hello/HelloController.java
@@ -1,12 +1,14 @@
 package ch.dfringeli.frigo.Hello;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
+    @CrossOrigin()
     public String index() {
-        return "Hello World!";
+        return "Hello Frigo!";
     }
 }
