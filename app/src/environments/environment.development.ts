15d501e0a2263051fae9e3f4cd80e5974a843727
dfringeli <dfringeli@outlook.com>
2025-07-22 22:52:23 +0200

    switched idp

--- app/src/environments/environment.development.ts
+++ app/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8081',
-    idpUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_5NRsDyg6x',
-    idpClientId: 'om6um3c3jr7n1jr9sr7l0meed',
-    idpLogoutUrl: 'https://us-east-15nrsdyg6x.auth.us-east-1.amazoncognito.com/logout?client_id=om6um3c3jr7n1jr9sr7l0meed&logout_uri=http://localhost:4200'
+    idpUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_ddeiU3lMi',
+    idpClientId: 'nbm241ku4tf9v0t1nmfedhnsv',
+    idpLogoutUrl: 'https://us-east-1ddeiu3lmi.auth.us-east-1.amazoncognito.com/logout?client_id=nbm241ku4tf9v0t1nmfedhnsv&logout_uri=http://localhost:4200'
 };
